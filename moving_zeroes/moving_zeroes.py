5a150d0c1b3c4a269e5ec9d1758662b62b3708a2
Asher Kobin <asherkobin@gmail.com>
2020-05-21 15:36:14 -0400

    all but sliding windows which I'm working on now

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,22 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+  # Sample input: [0, 3, 1, 0, -2]
+  # Expected output: 3
+  # Expected output array state: [3, 1, -2, 0, 0]
+  num_zeros = 0
+  new_arr = []
 
-    pass
+  for num in arr: # for each num in array, if 0, increment counter, else add to new array
+    if num == 0:
+      num_zeros += 1
+    else:
+      new_arr.append(num)
+
+  for _ in range(num_zeros): # append the count of zeros
+    new_arr.append(0)
+
+  return new_arr
 
 
 if __name__ == '__main__':
